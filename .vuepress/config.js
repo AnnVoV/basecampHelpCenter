d616c963432fe483437aaa1800a54c5efab476b5
zhenganran <ann.zar@alibaba-inc.com>
2019-10-31 20:18:47 +0800

    feat: add vuepress config

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: [
+        [
+            'vuepress-plugin-yuque', {
+            // 这里可以使用你自己的语雀 Repo
+            repoUrl: 'https://yuque.antfin-inc.com/bsc/qenbto',
+            html: true,
+        },
+        ],
+    ],
+};
